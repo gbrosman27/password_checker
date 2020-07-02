6b24319d15cacb6542cd267f524246b79bb46320
Greg Brosman <greg.brosman@gmail.com>
2020-07-01 21:20:04 -0700

    functions added

--- checker.py
+++ checker.py
@@ -1,7 +1,18 @@
 import requests
+import hashlib
 
-# request from api with hashed password (k-anonymity)
-url = 'https://api.pwnedpasswords.com/range/' + 'F1CEE'
-response = requests.get(url)
-print(response)
 
+def request_api_data(hashed_password):
+    # request from api with hashed password.
+    url = 'https://api.pwnedpasswords.com/range/' + hashed_password
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise RuntimeError(f"Error fetching: {response.status_code}, check api and try again.")
+    return response
+
+def pwned_api_check(password):
+    # check if password exists in API repsonse.
+    # sha1password = hashlib.sha1(password.encode(utf-8))
+    pass
+
+request_api_data('123')
