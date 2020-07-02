3c7c26e67fcee2a7329509a11fdc77954c5e4bd4
Greg Brosman <greg.brosman@gmail.com>
2020-07-01 23:47:49 -0700

    completed code.

--- checker.py
+++ checker.py
@@ -1,5 +1,6 @@
 import requests
 import hashlib
+import sys
 
 
 def request_api_data(hashed_password):
@@ -11,10 +12,31 @@ def request_api_data(hashed_password):
     return response
 
 
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
+
 def pwned_api_check(password):
     # check if password exists in api response.
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-    return sha1password
+    first5_char, remaining_char = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    return get_password_leaks_count(response, remaining_char)
 
 
-pwned_api_check('123')
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f"{password} was found {count} times. You should change your password.")
+        else:
+            print(f"{password} was not found. All good.")
+    return "done!"
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
